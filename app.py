993c6a3864f505a1e2c9fb1e8602e13d89d5a6d7
Aditya <adimail2404@gmail.com>
2023-09-19 03:08:28 +0530

    app.py

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask
+import os
+import psycopg2
+# from dotenv 
+
+CREATE_ROOM_TABLES = (
+    """CREATE TABLE IF NOT EXISTS rooms (id SERIAL PRIMARY KEY, name TEXT)"""
+)
 
 app = Flask(__name__)
+url = os.getenv("DB_URL")
+connection = psycopg2.connect(url)
 
 @app.route('/')
 def home():
