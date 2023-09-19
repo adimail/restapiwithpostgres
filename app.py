b1cb2ab86d6cb725419c2981c652f3eb12f598ec
Aditya <adimail2404@gmail.com>
2023-09-19 16:08:07 +0530

    get and post methods tested using postman

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 import os
 import psycopg2
 from dotenv import load_dotenv
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
 
 
 CREATE_ROOM_TABLES = (
@@ -24,13 +24,8 @@ INSERT_TEMP = (
 )
 
 
-GLOBAL_NUMBER_OF_DAYS = {
-    "query": "SELECT COUNT(DISTINCT DATE(date)) AS days FROM temperatures;"
-}
-
-GLOBAL_AVG = {
-    "query": "SELECT AVG(temperature) as average FROM temperatures;"
-}
+GLOBAL_NUMBER_OF_DAYS = "SELECT COUNT(DISTINCT DATE(date)) AS days FROM temperatures;"
+GLOBAL_AVG = "SELECT AVG(temperature) as average FROM temperatures;"
 
 
 load_dotenv()
@@ -41,7 +36,7 @@ connection = psycopg2.connect(url)
 
 app = Flask(__name__)
 
-@app.route("/api/api/room", methods=["POST"])
+@app.post("/api/room")
 def create_room():
     data = request.get_json()
     name = data["name"]
@@ -53,25 +48,38 @@ def create_room():
             room_id = cursor.fetchone()[0]
     return {"id": room_id, "message": f"Room {name} created."}, 201
 
-
-@app.route("/api/api/room", methods=["POST"])
+@app.post("/api/temperature")
 def add_temp():
     data = request.get_json()
-    temp = data["name"]
+    temp = data["temperatures"]
     room_id = data["room"]
 
     try:
         date = datetime.strptime(data["date"], "%m-%d-%y %H:%M:%S")
 
     except KeyError:
-        date = datetime.now(timezone.utc)
+        ist_offset = timedelta(hours=5, minutes=30)
+
+        utc_now = datetime.now(timezone.utc)
+
+        date = utc_now + ist_offset
 
     with connection:
         with connection.cursor() as cursor:
             cursor.execute(CREATE_TEMPS_TABLE)
             cursor.execute(INSERT_TEMP, (room_id, temp, date))
 
-    return {"message": "Temperature added"}, 201
+    return {"message": f"Temperature {temp} added on {date}"}, 201
+
+@app.get("/api/average")
+def get_global_avg():
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(GLOBAL_AVG)
+            average = cursor.fetchone()[0]
+            cursor.execute(GLOBAL_NUMBER_OF_DAYS)
+            days = cursor.fetchone()[0]
+    return {"Average": round(average,2 ), "Days": days}
 
 if __name__ == '__main__':
     app.run()
